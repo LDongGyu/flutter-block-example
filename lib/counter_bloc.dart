008eadcbde9df3f32c5252d22ed7013bce43c816
LDongGyu <ehdrbchd@naver.com>
2021-01-14 18:43:17 +0900

    feat: onChange, Error, Transition 추가

--- lib/counter_bloc.dart
+++ lib/counter_bloc.dart
@@ -22,4 +22,22 @@ class CounterBloc extends Bloc<CounterEvent, int>{
       yield state-1;
     }
   }
+
+  @override
+  void onChange(Change<int> change) {
+    print(change);
+    super.onChange(change);
+  }
+
+  @override
+  void onError(Object error, StackTrace stackTrace) {
+    print('$error, $stackTrace');
+    super.onError(error, stackTrace);
+  }
+
+  @override
+  void onTransition(Transition<CounterEvent, int> transition) {
+    print(transition);
+    super.onTransition(transition);
+  }
 }
